6b36b92e306d4cfc3ada296bdd109188e0faea7f
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2021-07-27 21:49:52 +0300

    add form typings

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export enum ErrorMessages {
+  UPPERCASE = "uppercase",
+  LOWERCASE = "lowercase",
+  ISNUMBER = "number",
+  CONTAINS = "conatins",
+}
+
+export type InitialValues = {
+  email: string;
+  password: string;
+};
