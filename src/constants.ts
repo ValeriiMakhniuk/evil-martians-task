f793fe738e419cb7ad2fc89b02004f07c1193dfd
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2021-07-28 21:58:17 +0300

    add constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,15 @@
+import { ErrorMessages } from "./types";
+
 export const ERROR_MESSAGES_DELIMETER = "_";
+export const MIN_PASSWORD_LENTGH = 8;
+
+export const passwordValidationStatus: Record<ErrorMessages, string> = {
+  [ErrorMessages.MINIMUM]: "more than 7 characters",
+  [ErrorMessages.CONTAINS]: "special character",
+  [ErrorMessages.ISNUMBER]: "number",
+  [ErrorMessages.LOWERCASE]: "lowercase character",
+  [ErrorMessages.UPPERCASE]: "uppercase character",
+};
 
 export const initialValues = {
   email: "",
