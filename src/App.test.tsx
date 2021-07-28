2bf89e38739f868001d87a7e812afdd66889663b
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2021-07-28 21:58:05 +0300

    add tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("form", () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  test("should be rendered", () => {
+    const linkElement = screen.getByTestId("login-form");
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("email input should have inital focus", () => {
+    const linkElement = screen.getByLabelText(/Email/i);
+    expect(linkElement).toHaveFocus();
+  });
+
+  test("email input should be empty", () => {
+    const linkElement = screen.getByLabelText(/Email/i);
+    expect(linkElement).not.toHaveValue();
+  });
 });
